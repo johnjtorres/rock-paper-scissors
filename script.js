4ceaaabe40c9d212ca6bf69e189d6231571a7bc2
John Torres <johnnyjamestorres@outlook.com>
2022-11-18 13:32:56 -0800

    Create function playRound, capitalize choices

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-const CHOICES = ['rock', 'paper', 'scissors'];
+const CHOICES = ['Rock', 'Paper', 'Scissors'];
 
 function getComputerChoice() {
   return CHOICES[Math.floor(Math.random() * 3)];
 }
 
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 function getPlayerChoice() {
   while (true) {
-    const playerChoice = prompt('Rock, Paper, or Scissors?').toLowerCase();
+    const playerChoice = capitalize(prompt('Rock, Paper, or Scissors?'));
     if (!CHOICES.includes(playerChoice)) continue;
     return playerChoice;
+  }
+}
+
+function playRound(computerChoice, playerChoice) {
+  switch (computerChoice.concat(playerChoice)) {
+    case 'RockPaper':
+    case 'PaperScissors':
+    case 'ScissorsRock':
+      return `You Win! ${playerChoice} beats ${computerChoice}!`;
+    case 'PaperRock':
+    case 'ScissorsPaper':
+    case 'RockScissors':
+      return `You Lose! ${computerChoice} beats ${playerChoice}!`;
+    default:
+      return "It's a tie!";
   }
 }
 
 const computerChoice = getComputerChoice();
 const playerChoice = getPlayerChoice();
 
-console.log(computerChoice);
-console.log(playerChoice);
+console.log(playRound(computerChoice, playerChoice));
